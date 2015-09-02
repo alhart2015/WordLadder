d8d33aaec01f6c15c2bc3fc32591d8ff11d66185
alhart2015 <alhart@davidson.edu>
2015-09-02 19:09:20 -0400

    Added comments

--- src/word_ladder_main.py
+++ src/word_ladder_main.py
@@ -1,29 +1,34 @@
-'''
+"""
 Main file for making a ladder between two words.
-'''
+"""
 
 import sys
 import WordGraph
 
+
 def make_ladder(first, second, debug):
-  '''Return a WordGraph from the first word to the second word.'''
+    """Return a WordGraph from the first word to the second word."""
 
-  if debug:
-    print 'Creating a word graph from "%s" to "%s"' % (first, second)
+    # There's lots of funny pep8 rules about how many empty lines and such to have. How interesting
+    # Also pycharm is kinda sick. You ever used intelliJ for java? It's the same company
 
-  graph = WordGraph.WordGraph()
+    if debug:
+        print 'Creating a word graph from "%s" to "%s"' % (first, second)
 
-  return graph
+    graph = WordGraph.WordGraph()
+
+    return graph
+
 
 if __name__ == '__main__':
-  debug = False
-  for arg in sys.argv:
-    if arg == "-d":
-      debug = True
+    debug = False
+    for arg in sys.argv:
+        if arg == "-d":
+            debug = True
 
-  first = raw_input('First word: ')
-  second = raw_input('Second word: ')
+    first = raw_input('First word: ')
+    second = raw_input('Second word: ')
 
-  graph = make_ladder(first, second, debug)
+    graph = make_ladder(first, second, debug)
 
-  print 'graph:', graph
+    print 'graph:', graph

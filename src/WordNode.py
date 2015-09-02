dee37e1a470993d801b7df809ab54687cbf114b0
alhart2015 <alhart@davidson.edu>
2015-09-01 22:24:31 -0400

    Added a test. Haven't figured out the overloading thing.

--- src/WordNode.py
+++ src/WordNode.py
@@ -15,17 +15,23 @@ class WordNode():
     def is_neighbor(self, other_word):
         """Returns true of the two words are different by one letter.
 
-        Parameter: word, a string to compare to the current node's word
+        :param other_word:  a string to compare to the current node's word
+        :rtype : bool
         Returns: true if the words differ by one letter
         """
+        differences = 0
         for i in range(len(self.__word)):
             if self.__word[i] != other_word[i]:
-                return False
-        return True
+                differences += 1
+        return differences == 1
 
-    def is_neighbor(self, other_node):
-        """Overload so that method works with strings and WordNodes"""
-        return self.is_neighbor(other_node.get_word())
+    # Overloading isn't supported in python and apparently is very non-pythonic. Any ideas how to check if a wordnode
+    # is neighbors with another node AND if it's neighbors with another node? Not that we NEED to, but it seems like
+    # the logical way to do things (maybe that's just my java talking)
+
+    # def is_neighbor(self, other_node):
+    #     """Overload so that method works with strings and WordNodes"""
+    #     return self.is_neighbor(other_node.get_word())
 
     def get_word(self):
         return self.__word
--- test/WordNodeTest.py
+++ test/WordNodeTest.py
@@ -1,4 +1,16 @@
 import unittest
+from WordNode import WordNode
 
 
+class WordNodeTest(unittest.TestCase):
+
+    def test_neighbor(self):
+        node = WordNode("beer")
+        yes = WordNode("deer")
+        no = WordNode("bend")
+        self.assertTrue(node.is_neighbor("deer"))
+        self.assertFalse(node.is_neighbor("bend"))
+
+if __name__ == "__main__":
+    unittest.main()
 

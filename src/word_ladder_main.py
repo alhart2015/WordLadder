3d29dff9248cbb94ecf122de521c476ad59b4fe7
Andrew Keesler <andrew.keesler@silabs.com>
2015-09-09 15:37:14 -0400

    Check in a broken main routine.
    - Lots of issues here.
      - The add_words function takes forever. I tried to cut some of
        the time off of it by having it only add words of a certain
        length, but it still takes a long time. The debug printing
        could probably be improved to update the user of its process,
        but I am too lazy to do that right now.
      - The actual path returned in the graph.path call in make_ladder
        returns None. I am definitely doing something wrong here.
        Hopefully I didn't blow something up.
    - Here are some sample sessions:
      - macbos0009550-2:WordLadder agkeesle [master] $ python
        src/word_ladder_main.py -d
        First word: man
        Second word: pan
        *** Creating a word graph from "man" to "pan"
        *** Adding words of length 3 from /usr/share/dict/words...
        *** Words added: 1420 (['aal', 'aam', 'aba', 'abb', 'Abe']...)
        *** Elapsed time: 1.453266 s
        path: None
      - macbos0009550-2:WordLadder agkeesle [master] $ python
        src/word_ladder_main.py -d
        First word: tuna
        Second word: fish
        *** Creating a word graph from "tuna" to "fish"
        *** Adding words of length 4 from /usr/share/dict/words...
        *** Words added: 5272 (['Aani', 'Aaru', 'abac', 'abas', 'Abba']...)
        *** Elapsed time: 19.795484 s
        path: None
    - I figured I would go ahead and check this in to get some review
      and help with the graph stuff.

--- src/word_ladder_main.py
+++ src/word_ladder_main.py
@@ -14,33 +14,59 @@ def debug_print(fmt, stuff, debug):
         print fmt % stuff
 
 
-def make_ladder(first, second, debug):
-    """Return a WordGraph from the first word to the second word."""
+def add_words(graph, word_length, word_file, debug):
+    """Adds the words found in word_file of length word_length to graph."""
+    debug_print('Adding words of length %d from %s...',
+                (word_length, word_file),
+                debug)
+
+    first_words = []
+    total_words = 0
+    with open(word_file) as input_file:
+        for word in input_file:
+            word = word.rstrip()
+            if len(word) == word_length:
+                graph.add_word(word)
+                total_words += 1
+                if len(first_words) < 5:
+                    first_words.append(word)
+
+    debug_print('Words added: %d (%s...)', (total_words, first_words), debug)
+
+
+def make_ladder(first, second, word_file, debug):
+    """Return a path from the first word to the second word."""
     debug_print('Creating a word graph from "%s" to "%s"',
                 (first, second), debug)
 
     start_time = time.time()
     graph = WordGraph()
+    add_words(graph, len(first), word_file, debug)
+    path = graph.path(first, second)
     end_time = time.time()
 
     debug_print('Elapsed time: %f s', (end_time - start_time), debug)
 
-    return graph
+    return path
 
 
 def main():
     """Program entry function."""
     debug = False
-    for arg in sys.argv:
-        if arg == "-d":
+    word_file = "/usr/share/dict/words"
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == "-d":
             debug = True
+        elif sys.argv[i] == "-f":
+            i += 1
+            word_file = sys.argv[i]
 
     first = raw_input('First word: ')
     second = raw_input('Second word: ')
 
-    graph = make_ladder(first, second, debug)
+    path = make_ladder(first, second, word_file, debug)
 
-    print 'graph:', graph
+    print 'path:', path
 
 
 if __name__ == '__main__':
